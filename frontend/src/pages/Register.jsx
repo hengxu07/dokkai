6485040a440516fd76c025789ab91485a5258471
Heng Xu <hengxu07@gmail.com>
2026-05-05 17:07:41 -0700

    update API URL to production

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const API = 'http://127.0.0.1:8000';
+const API = 'https://web-production-09a51.up.railway.app';
 
 function Register() {
   const [username, setUsername] = useState('');
